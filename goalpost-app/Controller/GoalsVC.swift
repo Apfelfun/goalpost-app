0e23575f982a73590ef15fa0e3bf84611c161c7c
Yousef Ouarghi <www_yousef@Yousefs-Mac-mini-3.local>
2017-08-05 14:20:04 +0200

    Video 5

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -8,23 +8,36 @@
 
 import UIKit
 
-class GoalsVC: UIViewController {
+class GoalsVC: UIViewController{
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let goal = Goal()
-        goal.goalProgress = 5
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
     }
     
+}
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
 }
 
---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Yousef Ouarghi on 05.08.17.
+//  Copyright © 2017 Yousef Ouarghi. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
