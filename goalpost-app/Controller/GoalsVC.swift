646501caad0d2e4cbcb138f4b62f53b0f21ef5fd
Yousef Ouarghi <www_yousef@Yousefs-Mac-mini-3.local>
2017-08-05 13:29:04 +0200

    Video 2

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goalpost-app
 //
 //  Created by Yousef Ouarghi on 05.08.17.
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+    }
+    
 }
 
